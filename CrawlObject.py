4749ddc8f579d1b69a127518d753b4b1d2a08f88
AntiNita <xiaoyeainta@gmail.com>
2022-04-15 17:30:12 -0400

    Completed the object!

--- CrawlObject.py
+++ CrawlObject.py
@@ -21,8 +21,9 @@ class Shanghai_Help_Scraper:
     def __init__(self):
         self.__curr_page = 1
         
+        
     # Constructs a URL
-    def __url(self, current_page): 
+    def __url(self, current_page):
         current_page = self.__curr_page
         str1="https://www.daohouer.com/index.php?page="
         str2=str(current_page)
@@ -38,23 +39,29 @@ class Shanghai_Help_Scraper:
     
     # Constructing the dataframe
     def __df(self, total_page):
+        rows = []
         while self.__curr_page <= total_page:
             xhtml = self.__scraper_content().decode('utf-8')
             p = HTMLTableParser()
             p.feed(xhtml)
             title = ['编号', '时间', '程度', '分类', '摘要', '地址', '详情']
-            rows = []
             for i in range(1, len(p.tables[0])):
                 rows.append(p.tables[0][i])
             self.__curr_page += 1
-        df = pd.DataFrame(columns = title, data = rows)    
-        return df 
+        df = pd.DataFrame(columns = title, data = rows)
+        return df
     
-    # Function call, users need to provide total_page 
+    # Function call, users need to provide total_page
     def get(self, total_page):
         df = self.__df(total_page)
         df.to_csv('shanghai_test.csv', encoding = 'gbk')
 
+    def pages_scraped(self):
+        return self.__curr_page - 1
+
 if __name__ == "__main__":
     test = Shanghai_Help_Scraper()
     test.get(5)
+    print("Congratulations, you scraped " + str(test.pages_scraped() + "pages!!"))
+
+        
